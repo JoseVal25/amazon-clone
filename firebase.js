c225d0b42621a196ab20c692b40ed245f2bbdbee
Jose valencia <javhvac25@outlook.com>
2023-01-15 15:58:15 -0800

    firebase and stripe relations config

--- firebase.js
+++ firebase.js
@@ -2,11 +2,19 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDn__eEHmS6WHri7awi392ushKi7mb1M1I",
-    authDomain: "amzn-clone-5b461.firebaseapp.com",
-    projectId: "amzn-clone-5b461",
-    storageBucket: "amzn-clone-5b461.appspot.com",
-    messagingSenderId: "240715647768",
-    appId: "1:240715647768:web:237f87262c3b7eeb8eec6d",
-    measurementId: "G-EBNDEX1MFL"
-  };
+  apiKey: "AIzaSyDn__eEHmS6WHri7awi392ushKi7mb1M1I",
+  authDomain: "amzn-clone-5b461.firebaseapp.com",
+  projectId: "amzn-clone-5b461",
+  storageBucket: "amzn-clone-5b461.appspot.com",
+  messagingSenderId: "240715647768",
+  appId: "1:240715647768:web:237f87262c3b7eeb8eec6d",
+  measurementId: "G-EBNDEX1MFL"
+};
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app
+
+const db = app.fireStore()
+
+export default db;
